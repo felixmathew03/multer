6a6ef424af70ac5d69be270fd04ea362fb5c88eb
felixmathew03 <felixmathewtt@gmail.com>
2024-10-22 16:54:12 +0530

    3

--- serverside/requestHandler.js
+++ serverside/requestHandler.js
@@ -1,22 +1,19 @@
 import userSchema from './models/user.model.js';
 
 
-export async function addUser(req,res) {
+export async function addUser(req, res) {
     try {
-        const image=req.file
+        const image = req.file;
         console.log(req.file);
-        const {email,username,phone}=req.body;
-        console.log(email,username,phone);
-        res.status(201).send({msg:"successs"})
-        userSchema
-            .create({email,username,phone,image})
-            .then(()=>{
-                return res.status(201).send({msg:"successs"})
-            })
-            .catch((error)=>{
-                return res.status(404).send({msg:"user not added"})
-            })
+        const { email, username, phone } = req.body;
+        console.log(email, username, phone);
+
+        // Create the user and send a response only after it's done
+        await userSchema.create({ email, username, phone, image });
+        res.status(201).send({ msg: "success" });
+        
     } catch (error) {
-        res.status(404).send(error);
+        console.error(error); // Optional: Log the error for debugging
+        res.status(403).send({ msg: "user not added" });
     }
 }
